81901e06bb3accd27f9696736fc4eb596bff66a9
kate-fsd <ekaterina.kryakushina@metalamp.io>
2021-10-11 14:29:05 +0300

    Build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,8 @@
 import { AxiosInstance, AxiosRequestConfig } from 'axios';
 import WebSocket from 'isomorphic-ws';
-import { FullReport, ContractStatus, ContractSchema } from './types';
+import { FullReport, ContractStatus, ContractSchema, AnyHaskellADT } from './types';
 /** Class representing a PAB (Plutus Application Backend) API. */
-export declare class Pab {
+export declare class Pab<Status extends AnyHaskellADT, StatusToState extends Record<Status['tag'], unknown>, EndpointsParams extends Record<string, unknown>> {
     axios: AxiosInstance;
     private sockets;
     private socketURL;
@@ -20,7 +20,7 @@ export declare class Pab {
      * Get full information about the PAB instance.
      * @return {Promise<Object>} - Promise fulfilled by the full report object.
      */
-    getFullReport: () => Promise<FullReport>;
+    getFullReport: () => Promise<FullReport<Status, StatusToState[Status['tag']]>>;
     /**
      * Activate contract.
      * @param {string} contractName - Contract name, that you get from calling `fullreport` or
@@ -34,13 +34,13 @@ export declare class Pab {
      * @param {string} contractInstanceId - Contract instance id.
      * @return {Promise<Object>} - Promise fulfilled by the contract instance's status object.
      */
-    getContractStatus: (contractInstanceId: string) => Promise<ContractStatus>;
+    getContractStatus: <K extends Status["tag"]>(contractInstanceId: string) => Promise<ContractStatus<Status, StatusToState[K]>>;
     /**
      * Get the contract instance's schema.
      * @param {string} contractInstanceId - Contract instance id.
      * @return {Promise<Object>} - Promise fulfilled by the contract instance's schema object.
      */
-    getContractSchema: (contractInstanceId: string) => Promise<ContractSchema>;
+    getContractSchema: (contractInstanceId: string) => Promise<ContractSchema<Status>>;
     /**
      * Call the contract instance's endpoint.
      * @param {string} contractInstanceId - Contract instance id.
@@ -49,7 +49,7 @@ export declare class Pab {
      *                        contracts and endpoints. Relate to `schema` endpoint to know about this
      *                        endpoint data structure.
      */
-    callContractEndpoint: (contractInstanceId: string, endpointName: string, data?: object) => Promise<void>;
+    callContractEndpoint: <EndpointName extends string>(contractInstanceId: string, endpointName: EndpointName, data?: EndpointsParams[EndpointName]) => Promise<void>;
     /**
      * Stop the contract instance.
      * @param {string} contractInstanceId - Contract instance id.
@@ -61,17 +61,17 @@ export declare class Pab {
      * @param {string} walletId - Wallet Id.
      * @return {Promise<Array>} - Promise fulfilled by the wallet's contracts statuses array.
      */
-    getContractsByWallet: (walletId: string) => Promise<ContractStatus[]>;
+    getContractsByWallet: (walletId: string) => Promise<ContractStatus<Status, StatusToState[Status['tag']]>[]>;
     /**
      * Get all contract instances statuses by all wallets.
      * @return {Promise<Array>} - Promise fulfilled by all wallets contracts statuses array.
      */
-    getContracts: () => Promise<ContractStatus[]>;
+    getContracts: () => Promise<ContractStatus<Status, StatusToState[Status['tag']]>[]>;
     /**
      * Get all contracts definitions.
      * @return {Promise<Array>}
      */
-    getContractsDefinitions: () => Promise<ContractSchema[]>;
+    getContractsDefinitions: () => Promise<ContractSchema<Status>[]>;
     /**
      * Set base WebSockets URL.
      * @param {string} url - Base URL for PAB WebSockets.
@@ -80,16 +80,28 @@ export declare class Pab {
     /**
      * Create WebSocket connection.
      * @param {string} [contractId=''] - Is optional. If contractId is passed, creates WebSocket
-     *                                   connection for this contract instance.
+     *                                   connection for this contract instance, else - creates
+     *                                   combined WebSocket connection.
      * @return - WebSocket instance.
      */
     createSocket: (contractId?: string) => WebSocket;
     /**
      * Return the WebSocket instance.
      * @param {string} [contractId=''] - Is optional. If contractId is passed, returns the WebSocket
-     *                                   instance for this contract instance or undefined.
+     *                                   instance for this contract instance or undefined, else -
+     *                                   returns the combined WebSocket instance.
      * @return - WebSocket instance or undefined.
      */
     getSocket: (contractId?: string) => WebSocket | undefined;
+    /**
+     * Add handler for event `message` for the WebSocket instance.
+     * @param {string} [contractId=''] - Is optional. If contractId is passed, adds handler to the
+     *                                   WebSocket instance by this contract instance, else - to the
+     *                                   combined WebSocket instance.
+     * @param {function} handleMessage - Callback for event `message`. Function with one argument,
+     *                                   witch is similar to `observableState` from `getContractStatus`
+     *                                   method;
+     */
+    addSocketMessageHandler: <K extends Status["tag"]>(contractId: string, handleMessage: (contents: StatusToState[K]) => void) => void;
 }
 export * from './types';
